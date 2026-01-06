33a19808bcd9190f86ab789f3565d77547ac4c5e
Solveig Langbakk <solveig.langbakk@digdir.no>
2026-01-06 12:09:38 +0100

    mer konsistent tekst paa feil og eksempler

--- src/main/java/no/idporten/eudiw/byob/service/api/ByobServiceAPISwaggerExamples.java
+++ src/main/java/no/idporten/eudiw/byob/service/api/ByobServiceAPISwaggerExamples.java
@@ -7,7 +7,7 @@ public class ByobServiceAPISwaggerExamples {
     private ByobServiceAPISwaggerExamples() {
 
     }
-    protected static final String SERVER_ERROR_EXAMPLE =  "{\"error\": \"server_error\", \"error_description\": \"Intern feil. Prøv igjen seinere\"}";
-    protected static final String NOT_FOUND = "{\"error\": \"User error\", \"error_description\": \"Ressursen ble ikke funnet. Prøv igjen med annen VCT\"}";
+    protected static final String SERVER_ERROR_EXAMPLE =  "{\"error\": \"internal_error\", \"error_description\": \"Internal_error_try_again_later\"}";
+    protected static final String NOT_FOUND = "{\"error\": \"client_error\", \"error_description\": \"not_found\"}";
 
 }
