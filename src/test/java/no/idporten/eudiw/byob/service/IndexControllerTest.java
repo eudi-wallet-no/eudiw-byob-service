5ce68c4816f01228a1eb1fb12970deecd4dee47a
Solveig Langbakk <solveig.langbakk@digdir.no>
2026-01-06 13:53:04 +0100

    fjerne ubrukt import

--- src/test/java/no/idporten/eudiw/byob/service/IndexControllerTest.java
+++ src/test/java/no/idporten/eudiw/byob/service/IndexControllerTest.java
@@ -2,7 +2,6 @@ package no.idporten.eudiw.byob.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springdoc.webmvc.ui.SwaggerWelcomeWebMvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
