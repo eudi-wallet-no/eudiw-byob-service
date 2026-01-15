19a560efd49e3cc97f87713a034d0e98c1358c7f
Randi Øyri <randi.oyri@digdir.no>
2026-01-15 08:30:48 +0100

    validering av exampledata med test

--- src/main/java/no/idporten/eudiw/byob/service/model/web/ExampleCredentialDataRequestResource.java
+++ src/main/java/no/idporten/eudiw/byob/service/model/web/ExampleCredentialDataRequestResource.java
@@ -2,14 +2,17 @@ package no.idporten.eudiw.byob.service.model.web;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
 import no.idporten.eudiw.byob.service.model.ExampleCredentialData;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record ExampleCredentialDataRequestResource(
-    @JsonProperty("name")
-    String name,
-    @JsonProperty("value")
-    String value
+        @NotEmpty
+        @JsonProperty("name")
+        String name,
+        @NotEmpty
+        @JsonProperty("value")
+        String value
 ){
     public ExampleCredentialData toExampleCredentialData() {
         return new ExampleCredentialData(
