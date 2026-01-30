3577d32a0a5bd870df2f77ac50762694da6d1e84
Thomas Reppesgård <thr@digdir.no>
2026-01-30 07:12:02 +0100

    Lik validering på eksempel claim som definisjon claim

--- src/main/java/no/idporten/eudiw/byob/service/model/web/ExampleCredentialDataRequestResource.java
+++ src/main/java/no/idporten/eudiw/byob/service/model/web/ExampleCredentialDataRequestResource.java
@@ -1,11 +1,12 @@
 package no.idporten.eudiw.byob.service.model.web;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.io.Serializable;
 import java.util.HashMap;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ExampleCredentialDataRequestResource extends HashMap<String, Serializable> {
+public class ExampleCredentialDataRequestResource extends HashMap<@NotEmpty(message = "Claim name must hava a value") String, Serializable> {
 
 }
