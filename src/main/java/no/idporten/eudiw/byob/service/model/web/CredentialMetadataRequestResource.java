3fc0ff0db9e056327ca521437a795c576b86924b
Anders Holst <anders.lietzen.holst@digdir.no>
2026-01-30 16:13:55 +0100

    Metadata request: display optional + list elements not null

--- src/main/java/no/idporten/eudiw/byob/service/model/web/CredentialMetadataRequestResource.java
+++ src/main/java/no/idporten/eudiw/byob/service/model/web/CredentialMetadataRequestResource.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import no.idporten.eudiw.byob.service.model.Claims;
 import no.idporten.eudiw.byob.service.model.CredentialMetadata;
 
@@ -14,13 +16,13 @@ import java.util.List;
 public record CredentialMetadataRequestResource(
         @Valid
         @JsonProperty("display")
-        @NotEmpty(message = "CredentialMetadata display is null or empty")
-        List<DisplayRequestResource> display,
+        @Size(min = 1, message = "CredentialMetadata display list is non-null and empty")
+        List<@NotNull(message = "CredentialMetadata display is null") DisplayRequestResource> display,
 
         @Valid
-        @NotEmpty(message = "CredentialMetadata claims is null or empty")
+        @NotEmpty(message = "CredentialMetadata claims list is null or empty")
         @JsonProperty("claims")
-        List<ClaimsRequestResource> claims
+        List<@NotNull(message = "CredentialMetadata claims is null") ClaimsRequestResource> claims
 ) {
     public CredentialMetadata toCredentialMetadata() {
         List<no.idporten.eudiw.byob.service.model.Display> displays = this.display == null ? Collections.emptyList() : this.display.stream().map(DisplayRequestResource::toDisplay).toList();
