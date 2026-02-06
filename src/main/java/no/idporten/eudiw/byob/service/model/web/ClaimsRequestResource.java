236d0df25cc558412c79a67e578857453f796532
Thomas Reppesgård <thr@digdir.no>
2026-02-06 13:55:49 +0100

    Vi krever display

--- src/main/java/no/idporten/eudiw/byob/service/model/web/ClaimsRequestResource.java
+++ src/main/java/no/idporten/eudiw/byob/service/model/web/ClaimsRequestResource.java
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import no.idporten.eudiw.byob.service.model.Claims;
 
 import java.util.List;
@@ -22,7 +22,7 @@ public record ClaimsRequestResource(
         @JsonProperty("mandatory")
         boolean mandatory,
         @Valid
-        @Size(min = 1, message = "Claims display list is non-null and empty")
+        @NotEmpty(message = "Claims display is null or empty")
         @JsonProperty("display")
         List<@NotNull(message = "Claims display is null") DisplayRequestResource> display
 ) {
