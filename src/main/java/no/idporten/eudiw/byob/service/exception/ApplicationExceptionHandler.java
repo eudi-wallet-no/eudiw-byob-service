4b88f9519ba3f5771df9842392b04bfd945db1eb
Thomas Reppesgård <thr@digdir.no>
2026-02-20 14:41:12 +0100

    Merge

--- src/main/java/no/idporten/eudiw/byob/service/exception/ApplicationExceptionHandler.java
+++ src/main/java/no/idporten/eudiw/byob/service/exception/ApplicationExceptionHandler.java
@@ -4,6 +4,7 @@ package no.idporten.eudiw.byob.service.exception;
 import no.idporten.eudiw.byob.service.model.web.CredentialConfigurationRequestResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
